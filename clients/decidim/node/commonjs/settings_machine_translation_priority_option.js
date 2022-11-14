f0853534837be9239b23c55c187411b9f0383049
Hadrien Froger <hadrien@octree.ch>
2022-11-14 11:06:26 +0000

    build

---
+++ clients/decidim/node/commonjs/settings_machine_translation_priority_option.js
@@ -0,0 +1,23 @@
+// source: decidim.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.voca_decidim.SETTINGS_MACHINE_TRANSLATION_PRIORITY_OPTION');
+
+/**
+ * @enum {number}
+ */
+proto.voca_decidim.SETTINGS_MACHINE_TRANSLATION_PRIORITY_OPTION = {
+  SETTINGS_MACHINE_TRANSLATION_PRIORITY_ORIGINAL: 0,
+  SETTINGS_MACHINE_TRANSLATION_PRIORITY_TRANSLATED: 1
+};
+
